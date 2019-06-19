2d4cb07c4a15509cb96632c1054146b02eb20e60
thuyngavo <lisa.vo49@gmail.com>
2019-06-18 20:00:05 -0500

    Updates

---
+++ precache-manifest.135b1699f48770ffc3911422d95209f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b69707bd7b8ab69ac1f7695bd644c03e",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "7d0bc710cdb800c622d6",
+    "url": "/clickyGame/static/css/main.630dcf50.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clickyGame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "7d0bc710cdb800c622d6",
+    "url": "/clickyGame/static/js/main.733addda.chunk.js"
+  },
+  {
+    "revision": "95f5ce002aae55a30c31",
+    "url": "/clickyGame/static/js/runtime~main.d5f62368.js"
+  }
+]);
